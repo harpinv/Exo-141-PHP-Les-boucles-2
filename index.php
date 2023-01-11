0d30acbcf43368b0bd6191d5289f1a38b6ff20b5
vince <vincent.harpin@orange.fr>
2023-01-11 21:57:17 +0100

    exercice 141

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+for($i = 0; $i < 10; $i++) {
+    echo $i;
+}
+
+$trou = 7;
+for($i = 0; $i < 20; $i++) {
+    echo $i * $trou;
+}
+
+$mouton = 24;
+for($i = 100; $i > 10; $i--) {
+    echo $i * $mouton;
+}
+
+for($i = 1; $i < 11; $i += 0.5) {
+    echo $i;
+}
+
+for($i = 1; $i < 16; $i++) {
+    echo "On y arrive presque";
+}
+
+for($i = 20; $i > -1; $i--) {
+    echo "C'est presque bon";
+}
+
+for($i = 1; $i < 101; $i += 15) {
+    echo "On tient le bon bout.";
+}
+
+for($i = 200; $i > -1; $i -= 12) {
+    echo "Enfin !!!!.";
+}
